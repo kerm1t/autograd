2359e0510f3bda8e77a9428dc86adee3278bc007
kerm1t <wolfgang.schulz@gmail.com>
2026-01-03 09:01:42 +0100

    neuron functionality

--- perceptron.py
+++ perceptron.py
@@ -5,7 +5,31 @@ Created on Fri Jan  2 11:21:24 2026
 @author: lidar
 """
 
-class perceptron:
+
+# loop over all input points
+#    - multiply with weigths
+#    - calculate cost
+
+class neuron:
 #    weights
 #    bias (activation or delta ist/soll)
-#    input 
+#    input
+    def __init__(self, weights, bias):
+        self.n = len(weights) 
+        self.weight = weights
+        self.bias = bias
+
+    def process(self, item):
+        z = 0
+        for i in range(self.n):
+            z += item[i]*self.weight[i]
+        return (z+self.bias > 0)
+    
+    
+#class perceptron:
+def tester():
+    n = neuron([1,2],1.5)
+
+
+n = neuron(weights=[1,2],bias=1.5)
+classe = n.process([1.1,4])
